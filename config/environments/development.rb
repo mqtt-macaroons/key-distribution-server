4a0134a8737439263198034bf357e1c20ed8b4ac
Prashant <prashant.barca@gmail.com>
2016-10-15 01:27:41 -0400

    Initial commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,8 @@ Rails.application.configure do
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => 'public, max-age=172800'
